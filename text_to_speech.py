9f5efe5891552cfdefab583f51894a8e822443b1
primeSR <shashankrai207@gmail.com>
2021-10-22 16:24:00 +0530

    try-catch block

--- text_to_speech.py
+++ text_to_speech.py
@@ -10,14 +10,15 @@ with open('textFile.txt','r',encoding='utf-8') as text_file:
     text_to_speech = gTTS(lines,tld='co.in')
     i = 1
     path = './audio/'
-    if len(os.listdir(path)) == 0:
-        text_to_speech.save('./audio/speech.mp3')
-    else:
-        try:
-            text_to_speech.save(f'./audio/speech{i}.mp3')
-            i+=1
-        except Exception as e:
-            print(e)
+    try:
+        if len(os.listdir(path)) == 0:
+            text_to_speech.save('./audio/speech.mp3')
+        else:
+
+                text_to_speech.save(f'./audio/speech{i}.mp3')
+                i+=1
+    except Exception as e:
+        print(e)
             
 
 
