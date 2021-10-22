32d686dbfb54b851d6da83f31b4c52d03a39fcee
primeSR <shashankrai207@gmail.com>
2021-10-22 15:30:30 +0530

    folder added

--- text_to_speech.py
+++ text_to_speech.py
@@ -9,14 +9,14 @@ with open('textFile.txt','r',encoding='utf-8') as text_file:
     # print(type(lines))
     text_to_speech = gTTS(lines,tld='co.in')
     i = 1
-    path = './'
+    path = './audio/'
     if len(os.listdir(path)) == 0:
-        text_to_speech.save('./speech.mp3')
+        text_to_speech.save('./audio/speech.mp3')
     else:
         # for file in os.listdir(path):
             # print(file)
             # if os.path.isfile(f'./audio/{file}.mp3'):
-            text_to_speech.save(f'./speech{i}.mp3')
+            text_to_speech.save(f'./audio/speech{i}.mp3')
             i+=1
             
 
