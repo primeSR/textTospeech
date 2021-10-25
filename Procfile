7c4294b9282c341d3ecd04ffff6c1dc5baf48719
primeSR <shashankrai207@gmail.com>
2021-10-25 17:15:31 +0530

    all audio files removed

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-
-web: gunicorn wsgi:app
+web: python webtts.py
+workers: python webtts.py
 
--- webtts.py
+++ webtts.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
-from flask import request, Response
+from flask import request
 # from flask.helpers import url_for
 from gtts import gTTS
 import os
 # from io import BytesIO
 # import dropbox
-# from flask_wtf.csrf import CSRFProtect
 
 
 
@@ -18,7 +17,6 @@ app = Flask(__name__)
 def web_tts():
   if request.method == "POST":
     lines = request.form.get('tts-text')
-    # lines = written.read()
     text_to_speech = gTTS(lines)
     # fp = BytesIO()
     # print(fp)
@@ -27,7 +25,7 @@ def web_tts():
     # access_token = "c1uzg7s-JewAAAAAAAAAAcXaywdcn4aOKrW93zXVsz2G0xOvr9BSSeinUbqzZofg"
     # dbx = dropbox.Dropbox(access_token)
     if len(os.listdir(path)) == 0:
-      text_to_speech.save('./audio/speech.mp3')
+      tts_file_obj = text_to_speech.save('./audio/speech.mp3')
       # response = Response(open("./audio/speech.mp3", "rb"), content_type='audio/mp3')
     # tts_file = text_to_speech.write_to_fp(fp) # <-- gtts bytes file object 
     # dbx.files_upload(text_to_speech.save('./speech.mp3'),'/audio')
@@ -35,7 +33,7 @@ def web_tts():
       for file in os.listdir(path):
                 # print(file)
         if os.path.isfile('./audio/speech.mp3'):
-          text_to_speech.save(f'./audio/speech{i}.mp3')
+          tts_file_obj = text_to_speech.save(f'./audio/speech{i}.mp3')
           # response = Response(open(f"./audio/speech{i}.mp3", "rb"), content_type='audio/mp3')
           # dbx.files_upload()
 
