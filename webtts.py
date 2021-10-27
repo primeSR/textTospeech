4ce6e4366023ed2d61eeb74d0ae62b518181c00f
primeSR <shashankrai207@gmail.com>
2021-10-27 16:20:50 +0530

    auto play

--- webtts.py
+++ webtts.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template
-from flask import request, send_file
-from flask.helpers import url_for
-# from flask.helpers import url_for
+from flask import request, send_file, redirect
 from gtts import gTTS
 import os
-# from io import BytesIO
-# import dropbox
 
-
-
-
+from io import BytesIO
+import audioplayer
 
 app = Flask(__name__)
+
+
+
+
+
 
 # port = int(os.getenv("PORT"))
 port = int(os.environ.get("PORT",5000))
@@ -19,44 +19,28 @@ port = int(os.environ.get("PORT",5000))
 def web_tts():
   if request.method == "POST":
     lines = request.form.get('tts-text')
+    fp = BytesIO()
     # print(lines)
+    # file = './static/tts.mp3'
     text_to_speech = gTTS(lines)
-    # print(gTTS(lines))
+    # text_to_speech.write_to_fp(fp)
+    text_to_speech.save('./static/tts.mp3')
+    audioplayer.AudioPlayer('./static/tts.mp3').play(block=True)
+    os.remove('./static/tts.mp3')
 
-    # i = 1
-    path = './static/'
-    # access_token = "c1uzg7s-JewAAAAAAAAAAcXaywdcn4aOKrW93zXVsz2G0xOvr9BSSeinUbqzZofg"
-    # dbx = dropbox.Dropbox(access_token)
-    # if len(os.listdir(path)) == 0:
-    text_to_speech.save('./static/speech.mp3')
-      # file = './static/speech.mp3'
-    text = f"audio file speech.mp3 generated"
-      # response = Response(open("./audio/speech.mp3", "rb"), content_type='audio/mp3')
-    # tts_file = text_to_speech.write_to_fp(fp) # <-- gtts bytes file object 
-    # dbx.files_upload(text_to_speech.save('./speech.mp3'),'/audio')
-    # else:
-    #   for file in os.listdir(path):
-    #             # print(file)
-    #     if os.path.isfile('./static/speech.mp3'):
-    #       text_to_speech.save(f'./static/speech{i}.mp3')
-    #       file = f'./static/speech{i}.mp3'
-    #       text = f"audio file speech{i}.mp3 generated"
-
-    #       # if os.path.isfile(f'./static/speech{i}.mp3'):
-    #       #   text = f"audio file speech{i}.mp3 generated"
-    #       i += 1
     
-    return render_template('index.html',text = text)
+    
+    # return render_template('index.html')
 
   return render_template('index.html')
 
-@app.route('/')
-def returnAudioFile():
-    path_to_audio_file = "./static/"
-    return send_file(
-         path_to_audio_file, 
-         mimetype="audio/mp3", 
-         as_attachment=True)
+# @app.route('/')
+# def returnAudioFile():
+#     path_to_audio_file = "./static/"
+#     return send_file(
+#          path_to_audio_file, 
+#          mimetype="audio/mp3", 
+#          as_attachment=True)
 
 
 if __name__ == '__main__':
